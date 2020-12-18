6ec9eb72c785a36b27b0407e1c1dbc3374b3cd26
Jiwung Hyun <kabbi159@gmail.com>
2020-12-18 17:27:46 +0900

    update baseline.py

--- baseline.py
+++ baseline.py
@@ -14,7 +14,6 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--max-epochs', type=int, default=2)
-    parser.add_argument('--device', type=str, default=None)
     parser.add_argument('--batch-size', type=int, default=4)
     parser.add_argument('--max-sequence-length', type=int, default=128)
 
@@ -33,6 +32,8 @@ def main():
     if args.wandb:
         wandb.init(project=args.model_name)
 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     # config, tokenizer, model
     config = AutoConfig.from_pretrained(
         args.model_name,
@@ -46,6 +47,7 @@ def main():
         args.model_name,
         config=config
     )
+    model.to(device)
 
     # load data
     train_loader, validation_loader, test_loader = load_datasets(args, tokenizer)
@@ -54,15 +56,25 @@ def main():
     optimizer = AdamW(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
 
     best_val = 0.
-    for i in range(args.max_epochs):
-        train(model, optimizer, train_loader, args)
-        val_acc = validation(model, validation_loader, args)
-        test_acc = test(model, test_loader, args)
+    for epoch in range(args.max_epochs):
+        train(model, optimizer, train_loader, args, device)
+        val_acc = validation(model, validation_loader, args, device)
+        test_acc = test(model, test_loader, args, device)
 
-        print(f"Epoch {i + 1} | val_acc: {val_acc} test_acc: {test_acc}")
+        print(f"Epoch {epoch + 1} | val_acc: {val_acc} test_acc: {test_acc}")
 
         if val_acc > best_val:
-            torch.save(model.state_dict(), os.path.join(args.save_dir, args.model_name + '.pt'))
+            os.makedirs(args.save_dir, exist_ok=True)
+            model_name = 'baseline_' + args.model_name + '.pt'
+            model_to_save = model.module if hasattr(model, 'module') else model
+            torch.save(dict(
+                epoch=epoch+1,
+                model_state_dict=model_to_save.state_dict(),
+                optimizer_state_dict=optimizer.state_dict(),
+                args=args
+            ),
+                os.path.join(args.save_dir, model_name)
+            )
             print("Model saved to", args.save_dir)
             best_val = val_acc
 
